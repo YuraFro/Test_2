c6fe6722da6c10340c55d9879b3ecbebab39a92f
Юрий Фролов <urafrolov0101@gmail.com>
2021-12-01 17:00:25 +0300

    Добавьте файлы проекта.

---
+++ Test_2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url1 = "http://uniserver.vesysoft.ru:8123/core/plugins/WeightIndicator1/Enable?Enable=true&auth_user=user&auth_password=user";
+            string url2 = "http://uniserver.vesysoft.ru:8123/core/SendMsg?Name=WeightIndicator1_GetParameters&auth_user=user&auth_password=user";
+
+
+            HttpWebRequest httpWebRequest1 = (HttpWebRequest)WebRequest.Create(url1);
+
+            HttpWebResponse httpWebResponse1 = (HttpWebResponse)httpWebRequest1.GetResponse();
+
+
+            HttpWebRequest httpWebRequest2 = (HttpWebRequest)WebRequest.Create(url2);
+
+            HttpWebResponse httpWebResponse2 = (HttpWebResponse)httpWebRequest2.GetResponse();
+
+            string response;
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse2.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+
+            var modul = JsonConvert.DeserializeObject<Moduls>(response);
+
+            textBox1.Text = modul.Massa.ToString();
+
+            //string url3 = "http://uniserver.vesysoft.ru:8123/core/SendMsg?Name=Camera1_Enable&Value=true&auth_user=user&auth_password=user";
+            string url4 = "http://uniserver.vesysoft.ru:8123/core/plugins/Camera1/Video?Width=596&Height=409&auth_user=user&auth_password=user";
+            //HttpWebRequest httpWebRequest1 = (HttpWebRequest)WebRequest.Create(url3);
+
+            //HttpWebResponse httpWebResponse1 = (HttpWebResponse)httpWebRequest1.GetResponse();
+
+
+            HttpWebRequest httpWebRequest3 = (HttpWebRequest)WebRequest.Create(url4);
+
+            HttpWebResponse httpWebResponse3 = (HttpWebResponse)httpWebRequest3.GetResponse();
+
+            //using (StreamReader streamReader = new StreamReader(httpWebResponse2.GetResponseStream()))
+            //{
+            //    pictureBox1 = streamReader.ReadToEnd();
+            //}
+
+            Bitmap b = new Bitmap(httpWebResponse3.GetResponseStream());
+
+            pictureBox1.Image = b;
+        }
+    }
+}
---
+++ Test_2/Moduls.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_2
+{
+    [Serializable]
+    public class Moduls
+    {
+        public bool Enable { get; set; }
+        public int State { get; set; }
+        public string StateName { get; set; }
+        public string UnitMeas { get; set; }
+        public int Precision { get; set; }
+        public int Discret { get; set; }
+        public string NameProtocol { get; set; }
+        public int Massa { get; set; }
+        public bool Stabil { get; set; }
+        public string StMassa { get; set; }
+        public string StState { get; set; }
+        public int RxPacket { get; set; }
+        public int RxByte { get; set; }
+        public int TxPacket { get; set; }
+        public int TxByte { get; set; }
+        public string WeightIndicatorPluginName { get; set; }
+        public string SerialPort { get; set; }
+        public string SerialPort_StState { get; set; }
+    }
+}
